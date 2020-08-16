1b1299539a36cfd6a9de7ecc1f3a79f79e7da240
Suvradeep <suvradeep.paria829@gmail.com>
2020-08-16 22:02:56 +0530

    data render

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,11 +1,39 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap';
+import axios from 'axios'
 
 class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            posts: []
+        }
+    }
+    
+    componentDidMount() {
+        axios.get('https://vidly-unique.herokuapp.com/api/users')
+        .then( response => {
+            console.log(response)
+            this.setState({posts: response.data})
+        })
+        .catch( error => {
+            console.log(error)
+        })
+    }
+
     render() {
+        const { posts } = this.state
         return (
             <div>
                 <Button variant="success">Log In</Button>
+                <h1>User list</h1>
+                {
+                    posts.length ?
+                    posts.map(post => <div key={post.id}> {post.name} </div>) :
+                    null
+                }
             </div>
         );
     }
